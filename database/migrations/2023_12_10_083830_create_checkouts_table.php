bf2a377afa5355dbe995a23c6fd9c68a26174345
Sourav Das <souravdas4816@gmail.com>
2023-12-11 03:36:43 +0530

    cascade delete of orders upon deleting user

--- database/migrations/2023_12_10_083830_create_checkouts_table.php
+++ database/migrations/2023_12_10_083830_create_checkouts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('from');
             $table->string('to');
             $table->bigInteger('weight');
