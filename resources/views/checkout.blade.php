a88600610435066d7a98218f92385ea1f02b5a7b
Sourav Das <souravdas4816@gmail.com>
2024-01-03 22:06:47 +0530

    update order status based on current location

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -25,7 +25,7 @@
 
                                 <div class="hidden">
                                     Booking person role:
-                                    <select class="grid-cols-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100" name='name'>
+                                    <select class="grid-cols-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100" name='role'>
                                         <option value="{{$roles}}">
                                             {{$roles}}
                                         </option>
